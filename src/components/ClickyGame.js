e4b13d52bc9f8d9d02ca7c5b3e502727c2863a1a
Kiara Guzman <kiara.kg11@gmail.com>
2019-10-26 02:44:28 +0800

    clickyGame js updated

--- src/components/ClickyGame.js
+++ src/components/ClickyGame.js
@@ -9,3 +9,19 @@ class ClickyGame extends Component {
   state = {
     score: 0,
     highScore: 0,
+    
+// stores the class value to assign to navMessage based on a good or bad click
+navMsgColor: '',
+
+// contains intro, success, and failure message
+navMessage: 'Click an image to begin!',
+
+// contains an array of image urls
+allCharacters: this.shuffleArray(),
+
+// will track  each clicked element.
+wasClicked: [],
+
+// shakes the container on an incorrect guess if set to true
+shake: false
+};
