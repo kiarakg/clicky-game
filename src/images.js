5c4ed16b86752ebf233bf60701dcd283cdec0a7c
Kiara Guzman <kiara.kg11@gmail.com>
2019-10-26 02:12:27 +0800

    more images added

--- src/images.js
+++ src/images.js
@@ -4,7 +4,12 @@ import bernieMac from './images/bernieMac.jpeg';
 import denzel from './images/denzel.jpeg';
 import gambino from './images/gambino.jpg';
 import idris from './images/idris.jpg';
-
+import jimCarey from './images/jimCarey.jpg';
+import leonardo from './images/leonardo.jpeg';
+import robinWilliams from './images/robinWilliams.jpeg';
+import samuel from './images/samuel.jpeg';
+import tracee from './images/tracee.jpeg';
+import willSmith from './images/willSmith.jpeg';
 
 const images = [
   bradPitt,
@@ -13,7 +18,12 @@ const images = [
   denzel,
   gambino,
   idris,
-
+  jimCarey,
+  leonardo,
+  robinWilliams,
+  samuel,
+  tracee,
+  willSmith
 ];
 
 export default images;
